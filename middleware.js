a6c7cbb4650b1737eabdd71f41f4be0c716999d7
Abbas W <abbas.code786@gmail.com>
2025-07-15 15:56:25 +0530

    feature added

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// p
+// pro
 const isProtectedRoute = createRouteMatcher([
   "/dashboard(.*)",
   "/account(.*)",
